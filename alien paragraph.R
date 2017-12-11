65e36a4ac2ac94593213de65164e23e8d36c5359
Jason Liao <jasonliao@Jasons-MBP.fios-router.home>
2017-12-10 21:06:22 -0500

    this is a paragraph for aliens

---
+++ alien paragraph.R
@@ -0,0 +1,5 @@
+print("Hi aliens, it is nice to meet you. I am the elected representative of earth,
+      and I'm supposed to be introducing you guys to our culture. We have a lot of cool
+      things on this planet, we have urban areas as well as rural areas, both are beautiful 
+      in their own way. If you guys are busy and only have time to visit one place, visit 
+      pittsburgh it's the best city in the world")
